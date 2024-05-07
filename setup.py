6de0a2661bb477aef851247e309cb562e1fea989
b1err <vilmokk@gmail.com>
2024-05-07 20:59:55 +0500

    id path

--- setup.py
+++ setup.py
@@ -1,2 +1,24 @@
-import subprocess
-subprocess.run(["pip", "install", "discord-rpc", "aiohttp"])
+import subprocess, os, importlib.util
+import configparser
+
+class generate_settings:
+    def setup(self):
+        subprocess.run(["pip", "install", "discord-rpc", "aiohttp"])
+
+    def read_user_id_from_config(self):
+        config_path = 'C:/RPC/settings.ini'
+        config = configparser.ConfigParser()
+        fid = input('Enter ForumID: ')
+        path = input('Enter gmodPath: ')
+        if not os.path.exists(config_path):
+            os.makedirs(os.path.dirname(config_path), exist_ok=True)
+        config.add_section('ForumID')
+        config.set('ForumID', 'ID', fid)
+        config.add_section('GmodPath')
+        config.set('GmodPath', 'Path', path)
+        with open(config_path, "w") as configfile:
+            config.write(configfile)
+
+if __name__ == '__main__':
+    generate_settings().setup()
+    generate_settings().read_user_id_from_config()
